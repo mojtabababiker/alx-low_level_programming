44232007e6f1f5ec7de157516dbc01758eef7da9
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-02-16 18:50:40 -0800

    Add task ?#100 ?#101 folder C hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 1 (success)
+ **/
+
+int main(void)
+{
+puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
