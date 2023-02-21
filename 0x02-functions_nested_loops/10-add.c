1ca875087b6be7c0ac02e87c315459ba3036912d
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-02-21 15:36:16 -0800

    Add task 10-add project 0x02

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adding two integer and return the result
+ * @x: arg1
+ * @y: arg2
+ * Return: the sum of the two integer
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
