a5b1f5ad443721b7eddbf54325cebcc54269a202
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-02-21 08:46:04 -0800

    Add task 11-print_to_98.c project 0x02

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+void down_to_limit(int n, int lim);
+void up_to_limit(int n, int lim);
+
+/**
+ * print_to_98 - print all numbers from the entered number to 98
+ *
+ * @n: user entered number
+ */
+void print_to_98(int n)
+{
+	int _limit = 98;
+
+	if (n > 98)
+		down_to_limit(n, 98);
+	else if (n < 98)
+		up_to_limit(n, 98);
+	else
+		printf("%d\n", _limit);
+}
+
+/**
+ * down_to_limit - when the enterd number is greater than 98 use this func
+ * @lim: local variable that holds the limit value
+ * @n: the number tht entered by the user
+ */
+
+void down_to_limit(int n, int lim)
+{
+	for (i = n ; i >= lim ; i--)
+	{
+		printf("%d", i);
+		if (i != lim + 1)
+			printf(", ");
+	}
+	putchar(10);
+}
+
+
+/**
+ * up_to_limit - when the enterd number is greater than 98 use this func
+ * @lim: local variable that holds the limit value
+ * @n: the number tht entered by the user
+ */
+
+void up_to_limit(int n, int lim)
+{
+	for (i = n ; i <= lim ; i++)
+	{
+		printf("%d", i);
+		if (i != lim - 1)
+			printf(", ");
+	}
+	putchar(10);
+}
