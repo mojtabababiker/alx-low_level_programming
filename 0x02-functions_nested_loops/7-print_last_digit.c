b2b745d198bc3ce78974202fd02cfc26ff648d83
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-02-21 16:13:32 -0800

    Edit 7-print_last_digit.c project 0x02

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,12 +10,16 @@ int print_last_digit(int num)
 {
 	int last_digit;
 	char i;
+	int j;
 
 	last_digit = num % 10;
-	for (i = 48 ; i <= 57 ; i++)
+	for (i = 48, j = 0 ; i <= 57 ; i++, j++)
 	{
-		if (last_digit == i)
+		if (last_digit == j)
+		{
 			_putchar(i);
+			break;
+		}
 	}
 	return (last_digit);
 }
