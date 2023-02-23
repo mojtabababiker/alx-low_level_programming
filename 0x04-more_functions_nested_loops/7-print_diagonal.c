8f2ddc0e86dfd6848ccd56e0d89ddff8dfaf3804
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-02-23 05:37:08 -0800

    Edit task 7-print_diagonal.c, Typo caused run-time-error

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,15 +12,15 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (i = 0 ; i <= n ; i++)
+		for (i = 0 ; i < n ; i++)
 		{
 			for (j = 0 ; j < i ; j++)
 			{
 				_putchar(' ');
 			}
-			_putchar('\\');
+			_putchar(92);
+			_putchar(10);
 		}
-		_putchar(10);
 	}
 	else
 	{
