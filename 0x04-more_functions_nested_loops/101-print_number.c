69773608370769b5b878f277b3a53188671b4d9d
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-02-23 06:11:31 -0800

    Add task 101-print_number project 0x04

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+void mult_digit(int n);
+
+/**
+ * print_number - Get a integer from the arg and print it
+ * @n: integer num
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_puthar('-');
+		n = -n;
+	}
+	if (n < 9)
+	{
+		_putchar(n + '0');
+	}
+	else if (n > 9)
+	{
+		mult_digit(n);
+	}
+	_putchar(10);
+}
+
+/**
+ * mult_digit - print multi digits numbers
+ * @n: the multi digit number
+ */
+
+void mult_digit(int n)
+{
+	if (n \ 10)
+		mult_digit(n \ 10);
+	_putchar(n % 10 + '0');
+}
