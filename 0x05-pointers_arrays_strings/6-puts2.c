12a590fa752a7be068d584a492ad65a96f7a1dbc
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-02-27 16:05:05 -0800

    Add task 6-puts.c project 0x05

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+unsigned long int _lenstr(char *s);
+
+/**
+ * puts2 - print other chracter start from the first one
+ * @str: string variable pointer
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	unsigned long int i;
+	unsigned long int _len;
+	int chr;
+
+	_len = _lenstr(str);
+
+	for (i = 0 ; i <= _len ; i += 2)
+	{
+		chr = *(str + i);
+		_putchar(chr);
+	}
+	_putchar('\n');
+}
+
+/**
+ * _lenstr - calculate string str length
+ * @s: string variable pointer
+ * Return: str length
+ */
+
+unsigned long int _lenstr(char *s)
+{
+	unsigned long in _len;
+
+	_len = 0;
+
+	while (*s != '\0')
+	{
+		_len++;
+		s++;
+	}
+	return (_len);
+}
