2cf37bceafccfc514945b6e1ebd71ac626b89724
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-04 00:44:01 -0800

    Edit task 100-rot13.c projcet 0x06

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -22,7 +22,7 @@ char *rot13(char *str)
 				*str = *str + 13;
 				break;
 			}
-		       	else if (*str >= 'n' && *str <= 'z')
+			else if (*str >= 'n' && *str <= 'z')
 			{
 				*str = *str - 13;
 				break;
