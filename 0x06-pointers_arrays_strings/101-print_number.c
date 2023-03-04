d48f25e06ada80c7e07fdf35172c4f38d4fdbb7c
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-04 06:01:41 -0800

    Edit task 101-print_number.c project 0x06

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include "main.h"
 
 /**
@@ -8,14 +9,17 @@
 
 void print_number(int n)
 {
+	unsigned int n1;
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -1 * n;
 	}
-	if (n > 9)
+	n1 = n;
+	if (n1 > 9)
 	{
-		print_number(n / 10);
+		print_number(n1 / 10);
 	}
-	_putchar((n % 10) +  '0');
+	_putchar((n1 % 10) +  '0');
+	return;
 }
