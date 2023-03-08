e794e08ff40af837fbb00f26b1ba993f244606f4
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-07 18:29:55 -0800

    Add task 3-strspn.c project 0x07

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,21 +3,33 @@
 /**
  * _strspn - gets the length of a prefix substring
  * @s: string to be scanned.
- *@accept: character to be scaned
+ * @accept: character to be scaned
  * Return: length of the matched charscters
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
 	int sum = 0;
+	char *temp;
+
+	temp = accept;
 
 	while (*s != '\0')
 	{
-		while (*accept != '\0' && *s == *accept)
+		while (*accept != '\0')
 		{
+			if (*s == *accept)
+			{
 				sum++;
 				s++;
 				accept++;
+			}
+			else
+			{
+				accept = temp;
+				sum = 0;
+				break;
+			}
 		}
 	}
 	return (sum);
