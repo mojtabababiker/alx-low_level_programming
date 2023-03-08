7a85c892318080fecc66d28a07f0c4d348a154c7
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-07 22:18:56 -0800

    Add task 1-print_rev_recursion.c project 0x08

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,10 +9,14 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
+	{
+		_putchar('\n');
 		return;
+	}
 	if (*(s + 1) == '\0')
+	{
 		return;
-
+	}
 	_print_rev_recursion(s + 1);
 	_putchar((int)*s);
 }
