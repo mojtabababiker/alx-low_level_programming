8b8e4e1da7ef50c13695ad286fee39e5a6acb9fb
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-08 16:11:19 -0800

    Add task 5-sqrt_recursion.c project 0x08

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * get_sqrt - calculate the square root for integer n
+ * using Repeated Subtraction Method
+ * @n: intger to claculate it's sqrt
+ * @odd: odd number to subtract
+ * Return: number of subtracting(sqrt)
+ */
+
+int get_sqrt(int n, int odd)
+{
+	int sum;
+
+	sum = 0;
+
+	if ((n - odd) == 0)
+	{
+		return (1);
+	}
+	else if ((n - odd) < 0)
+	{
+		return (-2147483648);
+	}
+	else
+	{
+		n = n - odd;
+		odd += 2;
+		return (sum + (get_sqrt(n, odd)));
+	}
+}
+
+/**
+ * _sqrt_recursion - calculate the square root for integer n
+ * @n: intger to claculate it's sqrt
+ * Return: n sqrt
+ */
+
+int _sqrt_recursion(int n)
+{
+	int _sqrt;
+
+	if (n < 0)
+		return (-1);
+
+	_sqrt = get_sqrt(n, -1);
+
+	if (_sqrt < 0)
+		return (-1);
+	else
+		return (_sqrt);
+}
