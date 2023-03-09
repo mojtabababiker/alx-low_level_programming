9584d7b40c930523d6ad1ff1c274a89d77d22c6e
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-09 03:08:35 -0800

    Edit task 6-is_prime_number.c project 0x08

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,85 +1,38 @@
 #include "main.h"
 
-int _sqrt_recour(int n);
-int is_prime(int n, int j);
+int primee(int n, int i);
 
 /**
- * is_prime_number - check if the number n is prime
- * @n: integer number to be checked
- * Return: 1=>prime 0=>other-wise
+ * is_prime_number - determine if the number is prime number or not
+ * @n: number to check
+ * Return: 1-prime 0-notprime
  */
-
 int is_prime_number(int n)
 {
-	int j;
-	int primee;
+	int prm;
 
-	j = _sqrt_recour(n);
-	primee = is_prime(n, j);
+	int i = 2;
 
-	return (primee);
+	prm = primee(n, i);
+	return (prm);
 }
 
 /**
- * is_prime - check if the number is prime
- * @n: number
- * @j: numbers from 2 to sqrt(n)
- * Return: indicator if 1 primw, 0 other-wise
+ * primee - check if number is prime
+ * @n: number to check
+ * @i: all numbers from 2 to n
+ * Return: 1-if prime 0-else
  */
 
-int is_prime(int n, int j)
+int primee(int n, int i)
 {
-	if (2 * j == n)
+	if (n == 0 || n == 1)
 		return (0);
-	if (j != 2)
-		is_prime(n, j - 1);
-	return (1);
-}
-
-/**
- * get_sqrt - calculate the square root for integer n
- * using Repeated Subtraction Method
- * @n: intger to claculate it's sqrt
- * @odd: odd number to subtract
- * Return: number of subtracting(sqrt)
- */
-
-int get_sqrt(int n, int odd)
-{
-
-	int sum;
-
-	odd += 2;
-	sum = 1;
-
-	if ((n - odd) == 0)
-	{
+	if (n == i)
 		return (1);
-	}
-	else if ((n - odd) < 0)
-	{
-		return (-2147483648);
-	}
-	else
-	{
-		sum = sum + get_sqrt(n - odd, odd);
-	}
-	return (sum);
-}
+	if (n % i == 0)
+		return (0);
 
-/**
- * _sqrt_recour - calculate the square root for integer n
- * @n: intger to claculate it's sqrt
- * Return: n sqrt
- */
-
-int _sqrt_recour(int n)
-{
-	int _sqrt;
-
-	if (n < 0)
-		return (-1);
-	_sqrt = get_sqrt(n, -1);
-
-	return (_sqrt);
+	i++;
+	primee(n, i);
 }
