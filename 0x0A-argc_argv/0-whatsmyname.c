8fd709917fd9a463e1ee2f0f680ad91ac7d7363f
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-10 11:43:31 -0800

    Add task 0-whatismyname.c project 0x0A

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,12 +4,10 @@
  * main - print the name of what the program called with
  * @argc: argumants count
  * @argv: list of passed args
- * Return: 0-seccuss 1-other-wise
  */
 
-int main(int argc, char *argv[])
+void main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
-	return (0);
 }
