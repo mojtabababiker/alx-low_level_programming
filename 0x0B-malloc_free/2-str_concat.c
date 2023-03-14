cc6823c20c6e62c98451eabe788f9a6ecdcfcbfd
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-13 17:03:54 -0700

    Add task 2-str_concat.c project 0x0B

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,77 @@
+#include "main.h"
+
+char *_strdup(char *s, unsigned int len, char *ptr);
+unsigned int _strlen(char *s);
+
+/**
+ * str_concat - concating two strings s1 and s2 in new memory location
+ * @s1: first string will concated to
+ * @s2: second string that wil be concated
+ * Return: pointer to the memory location that contains the result,
+ * NUll in fialure
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	unsigned int len1, len2;
+
+	len1  = _strlen(s1);
+	len2 = _strlen(s2);
+	ptr = (char *) malloc((len1 + len2) - 1);
+
+	if (ptr == NULL)
+		return (NULL);
+	if (s1 == NULL && s2 == NULL)
+	{
+		*ptr = '\0'
+		return (ptr);
+	}
+	if (s1 == NULL)
+		return (_strdup(s2, len2, ptr));
+	if (s2 == NULL)
+		return (_strdup(s1), len1, ptr);
+	for (i = 0 ; i < len1 - 1 ; i++)
+		ptr[i] = s1[i];
+	for (i = len1, j = 0 ; i < len2 ; i++, j++)
+		ptr[i] = s2[j];
+	ptr[len2] = '\0';
+
+	return (ptr);
+}
+
+/**
+ * _strlen - calculate s string length
+ * @s: string to calulate its length
+ * Return: s length
+ */
+
+unsigned int _strlen(char *s)
+{
+	static unsigned int len = 1;
+
+	if (s == NULL)
+		return (1);
+	if (!*s)
+		return (len);
+	len++;
+
+	return (_strlen(++s));
+}
+
+/**
+ * _strdup - duplicate s in new memory address
+ * @s: string to duplicate
+ * @len: string s length
+ * @ptr: new memory location pointer
+ * Return: pointer to the new memory location
+ */
+
+char *_strdup(char *s, unsigned int len, char *ptr)
+{
+	unsigned int i;
+
+	for (i = 0 ; i < len ; i++)
+		ptr[i] = s[i];
+	return (ptr);
+}
