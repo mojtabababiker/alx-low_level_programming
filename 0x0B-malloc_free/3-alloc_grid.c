a72e41fc82bb5169ad4e806bea7e87655acb2b8e
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-14 01:56:09 -0700

    Add task 3-alloc_gtid.c project 0x0B

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * alloc_grid - Allocate memory for 2D array
+ * @width: the number of arrays
+ * @height: numbers of elements in each arrays
+ * Return: pointer to 2D array location
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	array = (int **) malloc(sizeof(int) * height);
+	for (i = 0 ; i < width ; i++)
+	{
+		array[i] = (int *) malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+			return (NULL);
+	}
+	for (i = 0 ; i < height ; i++)
+		for (j = 0 ; j < width ; j++)
+			array[i][j] = 0;
+	return (array);
+}
