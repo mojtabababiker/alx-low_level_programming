5126f5f6a7d40d793bfcad2805a529f952a9ef0b
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-14 00:09:07 -0700

    Edit task 2-str_concat.c project 0x0B

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,7 +18,7 @@ char *str_concat(char *s1, char *s2)
 
 	len1  = _strlen(s1);
 	len2 = _strlen(s2);
-	ptr = (char *) malloc(sizeof(char) * ((len1 + len2)));
+	ptr = (char *) malloc(sizeof(char) * ((len1 + len2) - 6));
 
 	if (ptr == NULL)
 		return (NULL);
