e61d2315c1c97e067d2d28de075347ff96eb1c56
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-14 02:01:38 -0700

    Add task 3-alloc_gtid.c project 0x0B

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,6 +15,8 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	array = (int **) malloc(sizeof(int) * height);
+	if (*array == NULL)
+		return (NULL):
 	for (i = 0 ; i < width ; i++)
 	{
 		array[i] = (int *) malloc(sizeof(int) * width);
