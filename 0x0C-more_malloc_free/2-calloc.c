78f65bc4c0500a1f332b96fb3016c905ac34411e
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-16 16:10:59 -0700

    FINAL EDIT TASK 2-calloc.c PROJECT 0x0C

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0 ; i < nmemb * size ; i = i + size)
+	for (i = 0 ; i < nmemb ; i++)
 		ptr[i] = 0;
 	return ((void *)ptr);
 
