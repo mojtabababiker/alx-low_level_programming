3bfb14440745e5674ceaa409365ff1843106cc83
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-16 14:28:55 -0700

    Add task 2-calloc.c proect 0x0C

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _calloc - simulate calloc C stander function
+ * @nmemb: number of memoru blocks to allocate
+ * @size: size of each block
+ * @return: a void pointer to the base of allocated blocks
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0 ; i < nmemb * size ; i = i + size)
+		ptr[i] = 0;
+	return (ptr);
+}
