b04b61eb571cd48f77d5300d88d83193d802fca6
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-20 13:53:54 -0700

    Update header file dog.h, project 0x0E

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 /**
  * struct dog - Contains dog informations
  * @age: age of the dog in float point
@@ -19,4 +23,10 @@ struct dog
  */
 
 typedef struct dog dog_t;
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
