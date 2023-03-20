72d1b2bcd12877d50a68ded90e05be7783b90848
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-20 14:16:29 -0700

    Add task 2-print_dog.c, project 0x0E

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+
+/**
+ * print_dog - print dog sturct elemnts
+ * @d: pointer to dog struct
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %.1f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
