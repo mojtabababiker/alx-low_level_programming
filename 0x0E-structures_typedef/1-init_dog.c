5ced976a71ef89a3377de12b1e149200f06c8cca
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-20 13:34:19 -0700

    Finshing task 1-init_dig.c project 0x0E

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,15 @@
 
 /**
  * init_dog - use to initialize sturct dog elements
+ * @d: struct of dog
+ * @name: pointer for the name element in d struct
+ * @age: a copy of age element in d stuct
+ * @owner: pinter for the owner element in d struct
+ * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
 }
