e8ae3cc04be41e7b2b73902c91d18a0b6b285487
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-20 17:43:15 -0700

    ADD task from 0-4, project 0x0D

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the program source code name
+ * Return: 0-seccuss nonezero-other_wise
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef FUNC_LIKE_MICRO
+#define FUNC_LIKE_MICRO
+
+#define ABS(x) ((x) > 0 ? (x) : (x) * -1)
+#endif
