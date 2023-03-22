02e47949db2ec6ecb9d9f9602ca1f87bf0fefba7
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-22 15:45:00 -0700

    ADD task 1-array_iterator.c, project 0x0F

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - call action each time with one element from array
+ * @array: pointer to the args that will be passed to action
+ * @size: the number of the args in array
+ * @action: the callback function to be called
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0 ; i < size ; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
