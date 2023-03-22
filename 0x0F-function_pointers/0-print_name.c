79b39b21c36a73b6b7e3e4eb372d3a74d815c8ca
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-22 15:28:29 -0700

    Add task 0-print_name.c, project 0x0F

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print what ever is pointer nmae point to
+ *              using callback function
+ * @name: pointer to char array to be print
+ * @f: is the callback function
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
