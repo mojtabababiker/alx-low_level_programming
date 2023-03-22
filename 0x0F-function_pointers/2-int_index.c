2fa06230ca18cd0509347f1d843a11f921173a2d
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-22 16:19:55 -0700

    Add task 2-int_index.c, project 0x0F

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - search for certain integer number from array element
+ * @array: pointer to int array to search from
+ * @size: number of elements in array
+ * @cmp: pointer to compire function
+ * @Return: index of the wanted int, -1 if no element found
+ *          or size is less than or equal to 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (size == 0)
+		return (-1);
+	if ((array != NULL) && (cmp != NULL))
+	{
+		for (index = 0 ; index < size ; index++)
+		{
+			if (cmp(array[index]))
+				return (index);
+		}
+	}
+	return (-1);
+}
