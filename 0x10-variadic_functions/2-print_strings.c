d7b8efe0984de89428a8362cc104aef1254c4fc5
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-03-23 05:55:32 -0700

    Add task 2-print_string.c project 0x10-variadic_functions

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print string parameters and skip the rest
+ * @separator: word separator
+ * @n: number of parameters
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	unsigned int i;
+	char *st;
+
+	va_start(strings, n);
+	for (i = 0 ; i < n ; i++)
+	{
+		st = va_arg(strings, char *);
+
+		if (st == NULL)
+			printf("nil");
+		else
+			printf("%s", st);
+
+		if (separator != NULL && i != (n - 1))
+			printf("%s", separator);
+	}
+	putchar('\n');
+}
