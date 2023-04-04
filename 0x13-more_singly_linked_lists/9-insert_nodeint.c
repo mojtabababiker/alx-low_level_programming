662a8911446fa9432e32be815c93f1085a120222
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-04-04 16:35:16 -0700

    Edit task 9 project ox13, edit intteration logic

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *temp;
+	listint_t *new, *temp, *prev_node;
 
 	unsigned int i = 0;
 
@@ -19,15 +19,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	temp = *head;
-	for (i = 0 ; i < idx - 1 ; i++)
+	for (i = 0 ; i < idx ; i++)
 	{
 		if (*head == NULL)
 			return (NULL);
+		prev_node = *head;
 		*head = (*head)->next;
 	}
 	new->n = n;
-	new->next = (*head)->next;
-	(*head)->next = new;
+	new->next = (*head);
+	prev_node->next = new;
 	*head = temp;
 	return (new);
 }
