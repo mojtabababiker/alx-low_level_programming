3b3b1de2cbba8cbdf3cf45be5329e16c90b9c637
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-04-04 04:44:06 -0700

    Add task 5, project 0x13

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free listint_t list and all its elemnts
+ * @head: pointer to head node pointer
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	if (*head == NULL)
+		return;
+	free_listint2(&((*head)->next));
+	free(*head);
+}
