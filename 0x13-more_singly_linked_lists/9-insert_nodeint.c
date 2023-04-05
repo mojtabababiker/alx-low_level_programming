7c44daca09a4cdcc1960991dcc5e72e2e05324a3
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-04-04 17:36:49 -0700

    Edit task 9 priject 0x13, Handle memory leaks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * insert_nodint_at_inded - as the name say, insert node at a given position
+ * insert_nodeint_at_index - as the name say, insert node at a given position
  * @head: pointer to first node in the list
  * @idx: position to add the node on it
  * @n: the n value of the new node
@@ -18,21 +18,27 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	temp = *head;
 	for (i = 0 ; i < idx ; i++)
 	{
 		if (*head == NULL)
 		{
+			*head = temp;
 			free(new);
 			return (NULL);
 		}
 		prev_node = *head;
 		*head = (*head)->next;
 	}
-	new->n = n;
 	new->next = (*head);
 	prev_node->next = new;
-
 	*head = temp;
 	return (new);
 }
