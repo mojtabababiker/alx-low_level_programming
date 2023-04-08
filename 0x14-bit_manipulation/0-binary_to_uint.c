d71459541d245891784698d724a56b6d6a82430b
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-04-08 12:52:19 -0700

    Edit task 0 project 0x14, code style improved

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_unit - converts a binary number to an unsigned int
+ * binary_to_uint - converts a binary number to an unsigned int
  * @b: pointer to string of 0s and 1s
  * Return: the converted number or 0 if its failed
  */
@@ -12,13 +12,13 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 	len = strlen(b);
-	if (len > 32 || len <= 0)
+	if (len > 32 || len = 0)
 		return (0);
 	for (i = 0 ; i < len ; i++)
 	{
-		if (b[len - i - 1] != '0' && b[len - i - 1] != '1')
+		if (b[len - 1 - i] != '0' && b[len - 1 - i] != '1')
 			return (0);
-		else if  (b[len - i - 1] == '1')
+		else if  (b[len - 1 - i] == '1')
 			num = num + (1 << i);
 	}
 	return (num);
