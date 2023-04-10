4c804e5697358ff7cd8644fb504cb231ddcac4a7
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-04-10 07:48:00 -0700

    Add task 0 project 0x15

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile - read text file to stdout
+ * @filename: pointer to the file handler
+ * @letters: number of letters that printed
+ * Return: number of printed chars, or -1 if an error happend
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+
+	size_t i = 0;
+	ssize_t printed_chars = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (letters == 0)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)/*failed to open the file*/
+		return (-1);
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+		return (0);
+	/*init the values of bufer before use it*/
+	for (i = 0 ; i < letters ; i++)
+		buffer[i] = 0;
+	printed_chars = read(fd, buffer, letters);
+	printf("%s", buffer);
+	free(buffer);
+	close(fd);
+	return (printed_chars);
+}
