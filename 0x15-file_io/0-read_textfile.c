dc6c89ab920a4dfaa748e934ba71bdc9aa96a3b4
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-04-10 09:01:43 -0700

    Edit task 0 project 0x15, check the printed and the readed values

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,12 +15,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t buf_size = 0, printed_chars = 0;
 
 	if (filename == NULL)
-		return (-1);
+		return (0);
 	if (letters == 0)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)/*failed to open the file*/
-		return (-1);
+		return (0);
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
