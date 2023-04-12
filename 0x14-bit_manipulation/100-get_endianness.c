ceb476b08052ddd1d9a750d7b11ee3c05c094893
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-04-11 18:22:33 -0700

    Add task 100 project 0x15

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - determains if the system is LE or BE
+ * Return: 0 if BE or 1 LE
+ */
+int get_endianness(void)
+{
+	char *first_bit;
+
+	unsigned int multi_bytes = 0x8000;
+
+	first_byte = (char *) &multi_bytes;
+	if (first_bit)
+		return (0);
+	return (1);
+}
