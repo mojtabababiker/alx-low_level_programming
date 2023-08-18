63d288976adfce09142d64be5f962afb5d4db924
mojtabababiker <mojmohammad98@gmail.com>
2023-08-17 18:01:11 -0700

    Add the new version of the test case 7 and the bin file 7 project 0x17

--- 0x17-doubly_linked_lists/test/7-main.c
+++ 0x17-doubly_linked_lists/test/7-main.c
@@ -25,11 +25,12 @@ int main(void)
     head = NULL;
     printf("=============empty linked list case==\n");
     print_dlistint(head);
+    printf("-----------------\n");
     insert_dnodeint_at_index(&head, 0, 4096);
     print_dlistint(head);
     free_dlistint(head);
     head = NULL;
-    printf("=============node outrange case==\n");
+    printf("=============node index 0 case==\n");
     add_dnodeint_end(&head, 0);
     add_dnodeint_end(&head, 1);
     add_dnodeint_end(&head, 2);
@@ -39,10 +40,26 @@ int main(void)
     add_dnodeint_end(&head, 402);
     add_dnodeint_end(&head, 1024);
     print_dlistint(head);
-    insert_dnodeint_at_index(&head, 10, 4096);
+    printf("-----------------\n");
+    insert_dnodeint_at_index(&head, 0, 4096);
     print_dlistint(head);
     free_dlistint(head);
     head = NULL;
-    return (0);
+    printf("=============node last index case==\n");
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 3);
+    add_dnodeint_end(&head, 4);
+    add_dnodeint_end(&head, 98);
+    add_dnodeint_end(&head, 402);
+    add_dnodeint_end(&head, 1024);
+    print_dlistint(head);
+    printf("-----------------\n");
+    insert_dnodeint_at_index(&head, 8, 4096);
+    print_dlistint(head);
+    free_dlistint(head);
+    head = NULL;
 
+    return (0);
 }
