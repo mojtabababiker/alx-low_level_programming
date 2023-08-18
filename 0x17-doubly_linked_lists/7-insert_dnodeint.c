d3c5999121cce5ef06050d5840d2aa16e90c42a7
mojtabababiker <mojmohammad98@gmail.com>
2023-08-17 17:59:10 -0700

    Edit task 7 project 0x17, fix bugs and inhanced code style

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,27 @@
 #include "lists.h"
 
 /**
+ * insert_node - add node before the h node
+ * @h: the node that will be insert before
+ * @n: the data of the new node
+ * Return: a dlistin_t pointer of the new node, or NULL in failure
+ */
+dlistint_t *insert_node(dlistint_t **h, int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *h;
+	new->prev = (*h)->prev;
+	(*h)->prev->next = new;
+	(*h)->prev = new; /*set the new node as prev for head*/
+	return (new);
+}
+
+/**
  * insert_dnodeint_at_index - insert node at given postion
  * @h: pointer of the first node
  * @idx: the required index for the new node
@@ -14,40 +35,38 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	if (*h == NULL)
+	if (*h == NULL && idx == 0)
 	{
-		if (idx == 0)
-		{
-			new->next = NULL;
-			new->prev = NULL;
-			*h = new;
-			return (new);
-		}
-		else
-		{
-			free(new);
-			return (NULL);
-		}
+		new = add_dnodeint(h, n);
+		return (new);
 	}
+	else if (*h == NULL)
+		return (NULL);
 	temp = *h;
 	while (idx-- > 0)
 	{
 		if (*h == NULL)
 		{
 			*h = temp;
-			free(new);
 			return (NULL);
+		}
+		if ((*h)->next == NULL && idx == 0)
+		{
+			new = add_dnodeint_end(h, n);
+			*h = temp;
+			return (new);
 		}
 		*h = (*h)->next;
 	}
-	new->next = *h;
-	new->prev = (*h)->prev;
-	(*h)->prev->next = new;
-	(*h)->prev = new; /*set the new node as prev for head*/
+	if ((*h)->prev == NULL)
+	{
+		new = add_dnodeint(h, n);
+		if (new)
+			temp = new;
+		*h = temp;
+		return (new);
+	}
+	new = insert_node(h, n);
 	*h = temp;
 	return (new);
 }
