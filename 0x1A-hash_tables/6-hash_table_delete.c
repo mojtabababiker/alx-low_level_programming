6292d65d3264639d3f00dcf49168ca474fbcf81b
mojtabababiker <mojmohammad98@gmail.com>
2023-09-08 10:09:28 -0700

    Add task 5 project 0x1A

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete and free all the hash table nodes
+ * @ht: the hash table to be deleted
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t **arry = NULL, *temp = NULL;
+	unsigned long int i = 0;
+
+	arry = ht->array;
+	for (i = 0 ; i < ht->size ; i++)
+	{
+		while (arry[i] != NULL)
+		{
+			temp = arry[i]->next;
+			free_node(arry[i]);
+			arry[i] = temp;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
+
+/**
+ * free_node - free a single node
+ * @h_node: the node to be free'd
+ */
+void free_node(hash_node_t *h_node)
+{
+	h_node->next = NULL;
+	free(h_node->value);
+	free(h_node);
+}
