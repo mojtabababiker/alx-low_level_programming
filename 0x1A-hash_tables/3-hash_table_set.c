578ef1479b701a7681b18e286b7733ae212bcc4e
mojtabababiker <mojmohammad98@gmail.com>
2023-09-08 10:06:32 -0700

    Edit task 3 project 0x1A, fixe memory leaks bugs

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 
 	unsigned long int index;
-	hash_node_t *node, *h_node; /*, *prev;*/
+	hash_node_t *node;
 
 	if (ht == NULL || key == NULL || strlen(key) == 0)
 		return (0);
@@ -19,7 +19,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (node == NULL)
 		return (0);
 
-	/*prev = NULL;*/
 	node->key = (char *)key;
 	node->value = strdup(value);
 	if (node->value == NULL)
@@ -28,48 +27,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	index = key_index((unsigned char *)key, ht->size);
-	h_node = ht->array[index];
-	if (index == 0)
-		return (insert_node(h_node, node, ht, 0));
-
-	/**
-	 *prev = ht->array[index - 1];
-	 *while (prev != NULL && prev->next != NULL)
-	 *{
-	 *	prev = prev->next;
-	 *}
-	 *if (prev != NULL)
-	 *prev->next = node;
-	 */
-	insert_node(h_node, node, ht, index);
+	insert_node(node, ht, index);
 	return (1);
 }
 
 
 /**
- * free_node - free a single node
- * @h_node: the node to be free'd
- */
-void free_node(hash_node_t *h_node)
-{
-	h_node->next = NULL;
-	free(h_node->value);
-	free(h_node);
-}
-
-/**
  * insert_node - insert node in the array
- * @h_node: the node in the array[index] position
  * @node: the node to be inserted
  * @table: pointer to the hash table
  * @index: the index of the h_node
  * Return: 1 in success 0 otherwise
  */
-int insert_node(hash_node_t *h_node, hash_node_t *node,
-		hash_table_t *table, unsigned long int index)
+int insert_node(hash_node_t *node, hash_table_t *table,
+		unsigned long int index)
 {
-	hash_node_t *prev = NULL;
+	hash_node_t *prev = NULL, *h_node = NULL;
 
+	h_node = table->array[index];
 	if (h_node == NULL)
 	{
 		/* an empty spot on the array or update the Key*/
@@ -82,10 +57,11 @@ int insert_node(hash_node_t *h_node, hash_node_t *node,
 		if (strcmp(node->key, h_node->key) == 0)
 		{
 			node->next = h_node->next;
+			free_node(h_node);
 			if (prev != NULL)
 				prev->next = node;
-			free_node(h_node);
-			table->array[index] = node;
+			else
+				table->array[index] = node;
 			return (1);
 		}
 		prev = h_node;
